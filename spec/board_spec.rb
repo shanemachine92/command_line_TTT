79493cfe022ce7b1053c6b0f0937e1149f9f097b
Shane Zackery <shane@Shane-Zackerys-Macbook-Pro.local>
2017-06-20 17:37:45 -0700

    create spec files for game, board, player

---
+++ spec/board_spec.rb
@@ -0,0 +1,6 @@
+RSpec.describe Board do
+	it 'intializes an empty board'
+	it 'returns true if a cell is taken'
+	it 'records location of a piece'
+	it 'displays a piece in play'
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe Game do
+	it 'sets up the board'
+	it 'sets up players'
+	it 'switches players'
+	it 'checks for a win'
+	it 'checks for a draw'
+end
